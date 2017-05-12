0b39d50c65e079d16a056a253cbc4f6bfcce2487
suvpatil <suvit.patil@capgemini.com>
2017-05-12 16:34:30 +0530

    Update DataAccessLayer.go

--- DataAccessLayer.go
+++ DataAccessLayer.go
@@ -67,7 +67,7 @@ func getContractSpecificList(stub shim.ChaincodeStubInterface, contractId string
 		return contractList, errors.New("Failed to query table contractDetails")
 	}
 
-	json.Unmarshal(row.Columns[1].GetString_(), &contractList)
+	json.Unmarshal(row.Columns[0].GetString_(), &contractList)
 	return contractList, nil
 	
 }
